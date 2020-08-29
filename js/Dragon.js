bea8530c29a2139c7c18433c9f2a4fc7238bf279
monica326 <64248283+monica326@users.noreply.github.com>
2020-08-29 12:56:54 +0530

    Add files via upload

---
+++ js/Dragon.js
@@ -0,0 +1,13 @@
+class Dragon{
+    constructor(){
+        //background('white');
+        this.body = createSprite(100,100,20,20);
+        //this.mis = createSprite(player.x,player.y+50,20,8);
+          this.body.velocityX = 15;
+          this.body.addImage("this.body", dragon_image)
+          this.body.scale = 0.1;
+        
+        
+    }
+    
+}
